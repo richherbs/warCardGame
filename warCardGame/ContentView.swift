3d3863b476d677f5e3d95f92bd2924e22d3a5738
Richard Herbert <richard.herbert1980@gmail.com>
2021-09-10 20:48:08 +0100

    Initial Commit

---
+++ warCardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  warCardGame
+//
+//  Created by Richard Herbert on 10/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ warCardGame/warCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warCardGameApp.swift
+//  warCardGame
+//
+//  Created by Richard Herbert on 10/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct warCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
